9356aaea52df0995237a8b6dfe79bed4572bc443
unknown <Behaylu Gule>
2022-05-21 01:12:55 +0300

    correcting the news page

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -1,221 +1,114 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Headlines</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-	 <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
-	 <link href="css/element.css" rel="stylesheet">
-    <script src="js/jquery-2.1.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+  <title>AASTU queue management system</title>
+       
+    <!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap&subset=latin-ext" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="css/fontawesome-all.css" rel="stylesheet">
+    
+	<link href="css/magnific-popup.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">	
 
   </head>
   <body>
-@include('user.navbar')		 
-	    <section id="contact">
-		  <div class="container">
-		    <div class="row">
-			 <div class="col-sm-12 center_main">
-			  
-			  <div class="col-sm-10">
-			   <div class="clearfix">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 col-lg-12">
-                <h1 class="head_1">
-                    Contact us </h1>
-            </div>
-        </div>
-    </div>
+  @include('user.navbar')
+	        <!-- Top News Slider Start -->
+
+
+    <!-- News With Sidebar Start -->
+    <div class="container-fluid py-3" style="margin-top:100px">
+    @if(session()->has('message'))
+        <div class="alert alert-success">
+          {{session()->get('message')}}
+          <button type="button" class="close" data-dismiss="alert">
+            x
+          </button>
+        </div> 
+       @endif      
+       <!-- Wrapper container -->
+<div class="container py-4">
+
+<!-- Bootstrap 5 starter form -->
+<form id="contactForm" >
+
+  <!-- Name input -->
+  <div class="mb-3">
+	<label class="form-label" for="name">Name</label>
+	<input class="form-control" id="name" type="text" placeholder="Name" />
+  </div>
+
+  <!-- Email address input -->
+  <div class="mb-3">
+	<label class="form-label" for="emailAddress">Email Address</label>
+	<input class="form-control" id="emailAddress" type="email" placeholder="Email Address" />
+  </div>
+
+  <!-- Message input -->
+  <div class="mb-3">
+	<label class="form-label" for="message">Message</label>
+	<textarea class="form-control" id="message" type="text" placeholder="Message" style="height: 10rem;"></textarea>
+  </div>
+
+  <!-- Form submit button -->
+  <div class="d-grid">
+	<button class="btn btn-primary btn-sm" type="submit" style="color:black">Submit</button>
+  </div>
+
+</form>
+
 </div>
- 
-              <div class="col-md-8">
-            <div class="well well-sm">
-                <form>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="name">
-                                Name</label>
-                            <input type="text" class="form-control" id="name" placeholder="Enter name" required="required">
-                        </div>
-                        <div class="form-group">
-                            <label for="email">
-                                Email Address</label>
-                            <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
-                                </span>
-                                <input type="email" class="form-control" id="email" placeholder="Enter email" required="required"></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="subject">
-                                Subject</label>
-                            <select id="subject" name="subject" class="form-control" required="required">
-                                <option value="na" selected="">Select One:</option>
-                                <option value="service">Our Services</option>
-                                <option value="suggestions">Help & Suggestions</option>
-                                <option value="product">Product Enquiry</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="name">
-                                Message</label>
-                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required" placeholder="Message"></textarea>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
-                            Send Message</button>
-                    </div>
-                </div>
-                </form>
-            </div>
-        </div>
-              <div class="col-md-4">
-            <form>
-            <legend><span class="glyphicon glyphicon-globe"></span>&nbsp;Our Venue</legend>
-            <address>
-                <strong>Facebook, Inc.</strong><br>
-                767 Lorem ipsum, sit 400<br>
-                   consectetur, BA 10795<br>
-                <abbr title="Phone">
-                    C:</abbr>
-                (564) 453-9867
-            </address>
-            <address>
-                <strong>Full Name</strong><br>
-                <a href="#">info@gmail.com</a>
-            </address>
-            </form>
-        </div>
-  
+    </div>
+    
+    <!-- News With Sidebar End -->
 
-			  </div>
-			 </div>
-			</div>
-		  </div>
-		 </section>
+
+    <!-- Footer Start -->
+    @include('user.footer')
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-dark back-to-top"><i class="fa fa-angle-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    
+
 		 
-		<section id="footer">
-	   <div class="container">
-	     <div class="row">
-		   <div class="col-sm-12">
-		     <div class="col-sm-3">
-			   <div class="footer_1">
-			     <h3>ABOUT US</h3>
-				 <hr> 
-				 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-				 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-			   </div>
-			 </div>
-			 <div class="col-sm-3">
-			   <div class="footer_2">
-			      <h3>CONTACT INFO</h3>
-				 <hr>
-				 <p><i class="glyphicon glyphicon-map-marker"></i>4543k Integer, 7th block,<br><span class="city">Dolor Sit City.</span></p>
-				 <p><i class="glyphicon glyphicon-envelope"></i>info@gmail.com</p>
-				 <p><i class="glyphicon glyphicon-earphone"></i>+1234 567 890</p>
-			   </div> 
-			 </div>
-			 <div class="col-sm-3">
-			  <div class="footer_3">
-			     <h3>FLICKR POSTS</h3>
-				 <hr>
-				 <div class="col-sm-12">
-				   <div class="col-sm-4">
-				     <div class="footer_3_image"><a href="#"><img src="img/36.jpg"></a></div>
-				   </div>
-				   <div class="col-sm-4">
-				     <div class="footer_3_image"><a href="#"><img src="img/37.jpg"></a></div>
-				   </div>
-				   <div class="col-sm-4">
-				     <div class="footer_3_image"><a href="#"><img src="img/38.jpg"></a></div>
-				   </div>
-				 </div>
-				 <div class="col-sm-12">
-				   <div class="col-sm-4">
-				     <div class="footer_3_image"><a href="#"><img src="img/39.jpg"></a></div>
-				   </div>
-				   <div class="col-sm-4">
-				     <div class="footer_3_image"><a href="#"><img src="img/40.jpg"></a></div>
-				   </div>
-				   <div class="col-sm-4">
-				     <div class="footer_3_image"><a href="#"><img src="img/41.jpg"></a></div>
-				   </div>
-				 </div>
-				 <div class="col-sm-12">
-				   <div class="col-sm-4">
-				     <div class="footer_3_image"><a href="#"><img src="img/42.jpg"></a></div>
-				   </div>
-				   <div class="col-sm-4">
-				     <div class="footer_3_image"><a href="#"><img src="img/43.jpg"></a></div>
-				   </div>
-				   <div class="col-sm-4">
-				     <div class="footer_3_image"><a href="#"><img src="img/44.jpg"></a></div>
-				   </div>
-				 </div>
-			  </div>
-			 </div>
-			 <div class="col-sm-3">
-			  <div class="footer_4 clearfix">
-			    <h3>BLOG POSTS</h3>
-				 <hr>
-				 <div class="col-sm-12">
-				   <div class="col-sm-4">
-				     <div class="footer_4_image"><img src="img/45.jpg"></div>
-				   </div>
-				   <div class="col-sm-8">
-				     <div class="footer_4_para">
-					  <h4><a href="#">Nulla quis sem at nibh <br>Deserunt</h4></a><p>Posted On 15/4/2015</p>
-					 </div>
-				   </div>
-				 </div>
-				 <div class="col-sm-12">
-				   <div class="col-sm-4">
-				     <div class="footer_4_image"><img src="img/46.jpg"></div>
-				   </div>
-				   <div class="col-sm-8">
-				     <div class="footer_4_para">
-					  <h4><a href="#">Nulla quis sem at nibh <br>Deserunt</h4></a><p>Posted On 15/4/2015</p>
-					 </div>
-				   </div>
-				 </div>
-				 <div class="col-sm-12">
-				   <div class="col-sm-4">
-				     <div class="footer_4_image"><img src="img/47.jpg"></div>
-				   </div>
-				   <div class="col-sm-8">
-				     <div class="footer_4_para">
-					  <h4><a href="#">Nulla quis sem at nibh <br>Deserunt</h4></a><p>Posted On 15/4/2015</p>
-					 </div>
-				   </div>
-				 </div>
-			  </div>
-			 </div>
-		   </div>
-		 </div>
-	   </div>
-	 </section>
-	    
-		<section id="footer_bottom">
-	   <div class="container">
-	    <div class="row">
-		  <div class="col-sm-12">
-		    <p> © 2013 Your Website Name. All Rights Reserved | Design by <a href="http://www.templateonweb.com">TemplateOnWeb</a></p>
-		  </div>
-		</div> 
-	   </div>
-	 </section>
-
   <!-- //for header -->	
-    <script src="js/classie.js"></script>
-    <script src="js/cbpAnimatedHeader.js"></script>
+ 
   <!-- //for header_end -->	
-  </body>
-       
+  
+    <!-- //for slider -->
+  
+  <!-- //for slider_end -->
+  
+  <!-- //for header_end -->	
+  
+    <!-- //for slider -->
+
+  <script src="js/jquery.min.js"></> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="js/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
+    <script src="js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+    <script src="js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
+    <script src="js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
+    <script src="js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+    <script src="js/scripts.js"></script> <!-- Custom scripts -->
+   
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+	</body>
 </html>
