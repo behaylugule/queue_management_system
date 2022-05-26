55cc0a8111febd6cc48f2e4f4f2111856447e53d
unknown <Behaylu Gule>
2022-05-26 05:41:30 +0300

    update

--- resources/views/user/booking.blade.php
+++ resources/views/user/booking.blade.php
@@ -17,24 +17,17 @@
     <link rel="icon" href="images/favicon.png">
 
     <style>.section {
-    position: relative;
-    height: 100vh;
+  
 } 
 .section-center {
-    position: absolute;
-    top: 50%;
-    left: 0;
-    right: 0;
-    -webkit-transform: translateY(-50%);
-    transform: translateY(-50%);
+   
     
 }
 
 
 @media screen and (max-width: 600px) {
     .section-center {
-        position: absolute;
-        top: 400px;
+        
   }
 }
 
@@ -53,9 +46,8 @@
       
        
     @include('user.navbar')
-    <div class="container-fluid py-3" style="margin-top:100px">
+    <div class="container-fluid py-3" style="margin-top:130px">
       <div class="container mt-lg-4 mt-sm-0">
-        <div id="booking" class="section-center">
           <div class="container">
             @if(session()->has('message'))
               <div class="alert alert-success">
@@ -67,85 +59,81 @@
              @endif 
             <div class="row">
                 <div class="col-md-7 col-md-push-5">
-        <div class="table-responsive" id="book">       
-            <table class="table table-striped">
-            <thead>
-                <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Price(1)</th>
-                <th scope="col">quantity</th>
-                <th scope="col">Price(all qu)</th>
-                <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($user->items as $item)
-                <tr>
-                        <td>{{$item->name}}</td>
-                        <td>{{$item->price}}</td>
-                        <th>{{$item->pivot->quantity}}</th>
-                        <th>{{(float)$item->price * (int)$item->pivot->quantity}}</th>
-                        @if(!$book)
-                          <td>
-                              <a class="btn btn-danger" style="pointer-events:{{$book?'none':''}};background-color:{{$book?'gray':''}}"
-                          onclick="return  confirm('are u sure to remove this item from cart?')"
-                          href="{{url('remove_from_cart',$item->id)}}"  >Remove</a>
-                          </td>
-                        @endif
-                </tr>
-                @endforeach
-            </tbody>
-            <tr>
-                <th>Total Price</th>
-                <td>{{$totalPrice}}</td>
-                
-            </tr>
-            @if($book)
-            <tr>
-                <th>book status </th>
-                <td>your booking time</td>
-                <td>{{$booking->time->from}}AM-{{$booking->time->to}}AM </td>    
-            </tr>
-            @endif
-            </table>
-      </div>
-      @if($book)
-       <button class="btn btn-danger" onclick="printContent('book')">Print</button>
-      @endif
- </div>
- 
- @if(!$book)
-    <div class="col-md-4 col-md-pull-7">
-                
-     <div class="card" style="width: 28rem;">
-        <div class="card-body">
-          <h1 class="h2" align="center" style="margin-bottom:50px">Book Now</h1>
-         <form action="{{url('booking')}}" method="POST" enctype="multipart/form-data">
-                @csrf    
-                <div class="form-group">
-                    <label class="form-label" for='item'>Choose Time</label>
-                    <select class="form-select"  aria-label="select example" name='time' required style="width:auto">
-                        @foreach($times as $time)
-                        <option value="{{$time->id}}" {{($time->enable)?"":"disabled"}}>{{$time->from}}am -{{$time->to}}am LT {{$userPerHour->number}}</option>
+                  <div class="table-responsive" id="book">       
+                    <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price(1)</th>
+                        <th scope="col">quantity</th>
+                        <th scope="col">Price(all qu)</th>
+                        <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($user->items as $item)
+                        <tr>
+                                <td>{{$item->name}}</td>
+                                <td>{{$item->price}}</td>
+                                <th>{{$item->pivot->quantity}}</th>
+                                <th>{{(float)$item->price * (int)$item->pivot->quantity}}</th>
+                                @if(!$book)
+                                  <td>
+                                      <a class="btn btn-danger" style="pointer-events:{{$book?'none':''}};background-color:{{$book?'gray':''}}"
+                                  onclick="return  confirm('are u sure to remove this item from cart?')"
+                                  href="{{url('remove_from_cart',$item->id)}}"  >Remove</a>
+                                  </td>
+                                @endif
+                        </tr>
                         @endforeach
-                </div>
-                <div class="form-group" >
-                   <input type="submit"  {{($book)?"disabled":""}} class="btn btn-primary" style="background-color:#0275d8;margin-top:20px" value="Book" />
-               </div>
-            </form>
+                    </tbody>
+                    <tr>
+                        <th>Total Price</th>
+                        <td>{{$totalPrice}}</td>
+                        
+                    </tr>
+                    @if($book)
+                    <tr>
+                        <th>book status </th>
+                        <td>your booking time</td>
+                        <td>{{$booking->time->from}}AM-{{$booking->time->to}}AM </td>    
+                    </tr>
+                    @endif
+                    </table>
+                    </div>
+                    @if($book)
+                    <button class="btn btn-danger" onclick="printContent('book')">Print</button>
+                    @endif
+              </div>
+      @if(!$book)
+          <div class="col-md-4 col-md-pull-7">        
+          <div class="card" style="width: 28rem;">
+              <div class="card-body">
+                <h1 class="h2" align="center" style="margin-bottom:50px">Book Now</h1>
+              <form action="{{url('booking')}}" method="POST" enctype="multipart/form-data">
+                      @csrf    
+                      <div class="form-group">
+                          <label class="form-label" for='item'>Choose Time</label>
+                          <select class="form-select"  aria-label="select example" name='time' required style="width:auto">
+                              @foreach($times as $time)
+                              <option value="{{$time->id}}" {{($time->enable)?"":"disabled"}}>{{$time->from}}am -{{$time->to}}am LT {{$userPerHour->number}}</option>
+                              @endforeach
                       </div>
-                   </div>
-                </div>
-            </div>
-        </div>
-      @endif
+                      <div class="form-group" >
+                        <input type="submit"  {{($book)?"disabled":""}} class="btn btn-primary" style="background-color:#0275d8;margin-top:20px" value="Book" />
+                    </div>
+                  </form>
+                            </div>
+                        </div>
+                      </div>
+                      @endif
+              </div>
+          </div>
+        </div>   
     </div>
-      </div>
-      
-        {{-- @include('user.footer') --}}
+   </div>
+  @include('user.footer') 
      
-		 
-
   <!-- //for header -->	
   <script src="js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
